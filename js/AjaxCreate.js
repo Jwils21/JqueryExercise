be53b4a8dac9ddd32af36852224c6692e8464a06
Jonathan Wilson <jon.wilsond@gmail.com>
2018-06-27 13:48:00 -0400

    Added another exercise

--- js/AjaxCreate.js
+++ js/AjaxCreate.js
@@ -1,8 +1,75 @@
-function create() {
-    var id = document.getElementById("iId").value;
-    $.getJSON("http://192.168.50.253:58868/Customers/Get/"+id)
-        .done(function(resp){
+"option strict"
+
+function get() {
+    var id = document.getElementById("sId").value;
+    $.getJSON("http://localhost::580778/Users/Get/" + id)
+        .doe(function(resp) {
             console.log(resp);
-            display(resp);
+            load(resp);
         });
+}
+
+function load(jsonResponse) {
+    var user = jsonResponse.data;
+    document.getElementById("pId").value = user.id;
+    document.getElementById("pUsername").value = user.UserName;
+    document.getElementById("pPassword").value = user.Password;
+    document.getElementById("pFirstName").value = user.FirstName;
+    document.getElementById("pLastName").value = user.LastName;
+    document.getElementById("pEmail").value = user.Email;
+    document.getElementById("pPhone").value = user.Phone;
+    document.getElementById("pReviewer").value = user.isReviewer;
+    document.getElementById("pAdmin").value = user.isAdmin;
+    document.getElementById("pActive").value = user.isActive;
+
+}
+
+function change() {
+    var user = {
+        Id: 0,
+        UserName: Document.getElementById("pUsername").value,
+        Password: Document.getElementById("pPassword").value,
+        FirstName: Document.getElementById("pFirstName").value,
+        LastName: Document.getElementById("pLastName").value,
+        Phone: Document.getElementById("pPhone").value,
+        Email: Document.getElementById("pEmail").value,
+        Phone: Document.getElementById("pPhone").value,
+        isReviewer: Document.getElementById("pReviewer").value,
+        isAdmin: Document.getElementById("pAdmin").value,
+        isActive: Document.getElementById("pActive").value,
+    };
+
+        $.post("http://localhost:#####/Users/Change", user)
+            .done(function(resp) {
+                console.log(resp);
+                clearForm();
+        });
+
+}
+
+function remove() {
+    var user = {
+        Id: 0,
+        UserName: Document.getElementById("pUsername").value,
+        Password: Document.getElementById("pPassword").value,
+        FirstName: Document.getElementById("pFirstName").value,
+        LastName: Document.getElementById("pLastName").value,
+        Phone: Document.getElementById("pPhone").value,
+        Email: Document.getElementById("pEmail").value,
+        Phone: Document.getElementById("pPhone").value,
+        isReviewer: Document.getElementById("pReviewer").value,
+        isAdmin: Document.getElementById("pAdmin").value,
+        isActive: Document.getElementById("pActive").value,
+    };
+
+        $.post("http://localhost:#####/Users/Remove", user)
+            .done(function(resp) {
+                console.log(resp);
+                clearForm();
+        });
+
+}
+
+function clearForm() {
+    
 }
---
+++ js/jqueryTest.js
@@ -0,0 +1,16 @@
+"option strict"
+
+$().ready(function() {
+    $("#inpbox")
+        .val("This is default text in the input box")
+        .css("color","red")
+        .css("font-weight", "bold")
+
+
+    $("button").click(function() {
+        var txt = $("#inpbox").val();
+        $("label").text(txt);
+    });
+
+});
+
